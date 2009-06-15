85b0b8bcb69d4aeabe419dbf8e2dcb132dc731a9
Daniel Spiewak <djspiewak@apache.org>
2009-06-15 19:59:17 +0000

    Added custom Specs runner utility
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@784955 13f79535-47bb-0310-9956-ffa450edef68

---
+++ lib/buildr/scala/org/apache/buildr/SpecsSingletonRunner.scala
@@ -0,0 +1,33 @@
+package org.apache.buildr
+
+import java.net.{URL, URLClassLoader}
+import java.io.File
+
+/**
+ * @author Daniel Spiewak
+ */
+object SpecsSingletonRunner {
+  type Spec = { def main(args: Array[String]) }
+  
+  @throws(classOf[Throwable])
+  def main(args: Array[String]) {
+    val (colors, spec) = if (args.length > 1 && args(1) == "-c")
+      (true, args(2))
+    else
+      (false, args(1))
+    
+    run(args(0), colors, spec)
+  }
+  
+  @throws(classOf[Throwable])
+  def run(path: String, colors: Boolean, spec: String) = {
+    val parent = new File(path)
+    val specURL = new File(parent, spec.replace('.', '/') + ".class").toURL
+    val loader = new URLClassLoader(Array(specURL), getClass.getClassLoader)
+    
+    val clazz = loader.loadClass(spec)
+    val instance = clazz.getField("MODULE$").get(null).asInstanceOf[Spec]
+    
+    instance.main(if (colors) Array("-c") else Array())
+  }
+}
