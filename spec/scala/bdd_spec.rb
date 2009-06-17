096018a2b845bf502000438dbd17957adb5d056d
Daniel Spiewak <djspiewak@apache.org>
2009-06-17 15:28:07 +0000

    Removed obsolete test cases
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@785671 13f79535-47bb-0310-9956-ffa450edef68

--- spec/scala/bdd_spec.rb
+++ spec/scala/bdd_spec.rb
@@ -99,20 +99,6 @@ describe Buildr::Scala::Specs do
     lambda { define('foo').test.invoke }.should_not raise_error
   end
 
-  it 'should fail when ScalaTest test case fails' do
-    write 'src/spec/scala/FailingSpecs.scala', <<-SCALA
-      object FailingSpecs extends org.specs.Specification {
-        "it" should {
-          "add" in {
-            val sum = 1 + 1
-            sum mustEqual 42
-          }
-        }
-      }
-    SCALA
-    lambda { define('foo').test.invoke }.should raise_error(RuntimeError, /Tests failed/) rescue nil
-  end
-
   it 'should report failed test names' do
     write 'src/spec/scala/FailingSpecs.scala', <<-SCALA
       object FailingSpecs extends org.specs.Specification {
@@ -128,25 +114,6 @@ describe Buildr::Scala::Specs do
     project('foo').test.failed_tests.should include('FailingSpecs$')
   end
   
-  it 'should compile and run specifications with "Specs" suffix' do
-    write 'src/spec/scala/HelloWorldSpecs.scala', <<-SCALA
-      import org.specs._
-  
-      object HelloWorldSpecs extends Specification {
-        "'hello world' has 11 characters" in {
-          "hello world".size must beEqualTo(11)
-        }
-        "'hello world' matches 'h.* w.*'" in {
-          "hello world" must beMatching("h.* w.*")
-        }
-      }
-    SCALA
-    define('foo')
-    project('foo').test.invoke
-    project('foo').test.passed_tests.should include('HelloWorldSpecs$')
-  end
-
-
   it 'should fail if specifications fail' do
     write 'src/spec/scala/StringSpecs.scala', <<-SCALA
       import org.specs._
