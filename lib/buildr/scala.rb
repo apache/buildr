085ab961c2ff4cc8674a1a8c5f418205558838cf
Daniel Spiewak <djspiewak@apache.org>
2009-06-23 00:08:24 +0000

    Removed redundant shell definition
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@787493 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/scala.rb
+++ lib/buildr/scala.rb
@@ -22,34 +22,3 @@ require 'buildr/scala/tests'
 require 'buildr/scala/shell'
 
 Object::Scala = Buildr::Scala
-
-module Buildr
-  module ScalaShell
-    include Extension
-    
-    first_time do
-      Project.local_task 'shell'
-    end
-    
-    after_define do |project|
-      project.task 'shell' => :compile do
-        Scalac = Buildr::Scala::Scalac
-        
-        Scalac.scala_home or fail 'Are we forgetting something? SCALA_HOME not set.'
-        
-        cp = (project.compile.dependencies + Scalac.dependencies).join(File::PATH_SEPARATOR) +
-          project.path_to(:target, :classes)
-        
-        cmd_args = " -Denv.classpath='#{cp}'"
-        cmd_args += ' -classpath'
-        cmd_args += " '#{cp}'"
-        
-        system(File.expand_path('bin/scala', Scalac.scala_home) + cmd_args)
-      end
-    end
-  end
-  
-  class Project
-    include ScalaShell
-  end
-end
