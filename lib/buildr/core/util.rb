184485eed5067fbf102cd1f83d49f1672c3e4924
Daniel Spiewak <djspiewak@apache.org>
2009-06-23 00:00:36 +0000

    Should be overwriting Rake's `sh` rather than `system`
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@787452 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/core/util.rb
+++ lib/buildr/core/util.rb
@@ -336,18 +336,43 @@ if Buildr::Util.java_platform?
     end
   end
 
-  module Kernel
+  module FileUtils
     extend extend FFI::Library
     attach_function :system, [:string], :int
     alias_method :__native_system__, :system
     
-    def system(cmd, *args)
-      arg_str = args.map { |a| "'#{a}'" }
-      cd = "cd '#{Dir.pwd}' && "
-      back = __native_system__(cd + cmd + ' ' + arg_str.join(' ')) == 0
-      
-      $? = Buildr::ProcessStatus.new(0, back)    # KLUDGE
-      back
+    # code "borrowed" directly from Rake
+    def sh(*cmd, &block)
+      options = (Hash === cmd.last) ? cmd.pop : {}
+      unless block_given?
+        show_command = cmd.join(" ")
+        show_command = show_command[0,42] + "..."
+        
+        block = lambda { |ok, status|
+          ok or fail "Command failed with status (#{status.exitstatus}): [#{show_command}]"
+        }
+      end
+      if RakeFileUtils.verbose_flag == :default
+        options[:verbose] = false
+      else
+        options[:verbose] ||= RakeFileUtils.verbose_flag
+      end
+      options[:noop]    ||= RakeFileUtils.nowrite_flag
+      rake_check_options options, :noop, :verbose
+      rake_output_message cmd.join(" ") if options[:verbose]
+      unless options[:noop]
+        cd = "cd '#{Dir.pwd}' && "
+        res = if windows_os? && cmd.size == 1
+          __native_system__("#{cd} call #{cmd.first}") == 0
+        else
+          arg_str = args.map { |a| "'#{a}'" }
+          __native_system__(cd + cmd + ' ' + arg_str.join(' ')) == 0
+        end
+        $? = Buildr::ProcessStatus.new(0, res)    # KLUDGE
+        
+        block.call(res, $?)
+      end
     end
+
   end
 end
