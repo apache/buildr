4308d892dc8ce1c19d8f83e6e7a283ee39635266
Daniel Spiewak <djspiewak@apache.org>
2009-06-23 00:02:18 +0000

    Separators are nice to have
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@787463 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/scala/shell.rb
+++ lib/buildr/scala/shell.rb
@@ -19,12 +19,13 @@ module Buildr
         Scalac.scala_home or fail 'Are we forgetting something? SCALA_HOME not set.'
         
         cp = (project.compile.dependencies + Scalac.dependencies).join(File::PATH_SEPARATOR) +
-          project.path_to(:target, :classes)
+          File::PATH_SEPARATOR + project.path_to(:target, :classes)
         
         cmd_args = " -Denv.classpath='#{cp}'"
         cmd_args += ' -classpath'
         cmd_args += " '#{cp}'"
         
+        trace "scala #{cmd_args}"
         system(File.expand_path('bin/scala', Scalac.scala_home) + cmd_args)
       end
     end
