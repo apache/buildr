841783bb13fdd1ea12d33a1c6b01f08e615bde1e
Daniel Spiewak <djspiewak@apache.org>
2009-06-23 00:06:54 +0000

    Use JRUBY_HOME if available (for performance)
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@787485 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/core/shell.rb
+++ lib/buildr/core/shell.rb
@@ -8,6 +8,8 @@ module Buildr
       
       JRUBY_VERSION = '1.1.6'
       
+      SUFFIX = if Util.win_os? then '.bat' else '' end
+      
       class << self
         def lang
           :none
@@ -15,18 +17,33 @@ module Buildr
       end
       
       def launch
-        cp = project.compile.dependencies + [
-            "org.jruby:jruby-complete:jar:#{JRUBY_VERSION}",
-            project.path_to(:target, :classes)
-          ]
-        
-        Java::Commands.java 'org.jruby.Main', '--command', 'irb', {
-          :properties => rebel_props(project),
-          :classpath => cp,
-          :java_args => rebel_args
-        }
+        if jruby_home     # if JRuby is installed, use it!
+          cp = project.compile.dependencies.join(File::PATH_SEPARATOR) + 
+            File::PATH_SEPARATOR + project.path_to(:target, :classes)
+          
+          cp_var = ENV['CLASSPATH']
+          if cp_var
+            ENV['CLASSPATH'] += File::PATH_SEPARATOR
+          else
+            ENV['CLASSPATH'] = ''
+          end
+          ENV['CLASSPATH'] += cp
+          
+          trace "Running jirb using JRUBY_HOME: #{jruby_home}"
+          system(File.expand_path('bin/jirb' + SUFFIX, jruby_home))
+        else
+          cp = project.compile.dependencies + [
+              "org.jruby:jruby-complete:jar:#{JRUBY_VERSION}",
+              project.path_to(:target, :classes)
+            ]
+          
+          Java::Commands.java 'org.jruby.Main', '--command', 'irb', {
+            :properties => rebel_props(project),
+            :classpath => cp,
+            :java_args => rebel_args
+          }
+        end
       end
-      
     private
       def jruby_home
         @home ||= ENV['JRUBY_HOME']
