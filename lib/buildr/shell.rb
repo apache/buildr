b02d514157d5a16ceb333df6c6e14f5eaec50883
Daniel Spiewak <djspiewak@apache.org>
2009-06-23 00:03:20 +0000

    Added shell configuration (shell.using ...)
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@787468 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/shell.rb
+++ lib/buildr/shell.rb
@@ -20,6 +20,30 @@ module Buildr
   end
   
   module Shell
+    class Base
+      attr_reader :project
+      
+      class << self
+        def lang
+          :none
+        end
+        
+        def to_sym
+          @symbol ||= name.split('::').last.downcase.to_sym
+        end
+      end
+      
+      def initialize(project)
+        @project = project
+      end
+      
+      def launch
+        fail 'Not implemented'
+      end
+    end
+  end
+  
+  module ShellExtension
     include Extension
     
     first_time do
@@ -47,8 +71,7 @@ module Buildr
     end
     
     after_define do |project|
-      lang = project.compile.language
-      default_shell = ShellProviders.providers[lang]
+      default_shell = project.shell.using
       
       if default_shell
         dep = "shell:#{default_shell.to_sym}"
@@ -57,35 +80,38 @@ module Buildr
         project.task :shell => dep
       else
         project.task :shell do
-          fail "No shell provider defined for language '#{lang}'"
+          fail "No shell provider defined for language '#{project.compile.language}'"
         end
       end
     end
     
-    class Base
-      attr_reader :project
-      
-      class << self
-        def lang
-          :none
-        end
-        
-        def to_sym
-          @symbol ||= name.split('::').last.downcase.to_sym
-        end
-      end
-      
+    class ShellConfig
       def initialize(project)
         @project = project
       end
       
-      def launch
-        fail 'Not implemented'
+      def using(*args)
+        if args.size > 0
+          @using ||= args.first
+        else
+          @using ||= find_shell_task
+        end
       end
+      
+    private
+      def find_shell_task
+        lang = @project.compile.language
+        ShellProviders.providers[lang]
+      end
+    end
+    
+    # TODO  temporary hack
+    def shell
+      @shell ||= ShellConfig.new self
     end
   end
   
   class Project
-    include Shell
+    include ShellExtension
   end
 end
