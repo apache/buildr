8cba3957742fe9d001550aa7d73deeb0777ccdd8
Daniel Spiewak <djspiewak@apache.org>
2009-06-23 00:01:11 +0000

    Added prototype implementation of scala:shell task
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@787456 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/scala.rb
+++ lib/buildr/scala.rb
@@ -22,3 +22,30 @@ require 'buildr/scala/tests'
 require 'buildr/scala/bdd'
 
 Object::Scala = Buildr::Scala
+
+module Buildr
+  module ScalaProject
+    include Extension
+    
+    after_define do |project|
+      project.task 'scala:shell' => :compile do
+        Scalac = Buildr::Scala::Scalac
+        
+        Scalac.scala_home or fail 'Are we forgetting something? SCALA_HOME not set.'
+        
+        cp = (project.compile.dependencies + Scalac.dependencies).join(File::PATH_SEPARATOR) +
+          project.path_to(:target, :classes)
+        
+        cmd_args = " -Denv.classpath='#{cp}'"
+        cmd_args += ' -classpath'
+        cmd_args += " '#{cp}'"
+        
+        system(File.expand_path('bin/scala', Scalac.scala_home) + cmd_args)
+      end
+    end
+  end
+  
+  class Project
+    include ScalaProject
+  end
+end
