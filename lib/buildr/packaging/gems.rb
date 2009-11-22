4daa8ddd49ff571c08f4ca365b645d6f0f676038
Alex Boisvert <boisvert@apache.org>
2009-11-22 17:14:14 +0000

    Fix Gem::Package argument
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@883103 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/packaging/gems.rb
+++ lib/buildr/packaging/gems.rb
@@ -59,13 +59,17 @@ module Buildr
     def create_from(file_map)
       spec.mark_version
       spec.validate
-      Gem::Package.open(name, 'w', nil) do |pkg|
-        pkg.metadata = spec.to_yaml
-        file_map.each do |path, content|
-          next if content.nil? || File.directory?(content.to_s)
-          pkg.add_file_simple(path, File.stat(name).mode & 0777, File.size(content.to_s)) do |os|
-            File.open(content.to_s, "rb") do |file|
-              os.write file.read(4096)  until file.eof?
+      
+      File.open(name, 'w') do |io|
+        Gem::Package.open(io, 'w', nil) do |pkg|
+          pkg.metadata = spec.to_yaml
+          file_map.each do |path, content|
+            p "path #{path} content #{content}"
+            next if content.nil? || File.directory?(content.to_s)
+            pkg.add_file_simple(path, File.stat(content.to_s).mode & 0777, File.size(content.to_s)) do |os|
+              File.open(content.to_s, "rb") do |file|
+                os.write file.read(4096)  until file.eof?
+              end
             end
           end
         end
