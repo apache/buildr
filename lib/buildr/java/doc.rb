43d9bbab145aad5d5280a6a4a1619a8a7777c619
Daniel Spiewak <djspiewak@apache.org>
2010-02-20 18:10:17 +0000

    Created generic multi-language documentation framework
    * Ported the javadoc task
    * Added a scaladoc implementation
    
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@912175 13f79535-47bb-0310-9956-ffa450edef68

---
+++ lib/buildr/java/doc.rb
@@ -0,0 +1,55 @@
+require 'buildr/core/doc'
+
+module Buildr
+  module Doc
+
+    # A convenient task for creating Javadocs from the project's compile task. Minimizes all
+    # the hard work to calling #from and #using.
+    #
+    # For example:
+    #   doc.from(projects('myapp:foo', 'myapp:bar')).using(:windowtitle=>'My App')
+    # Or, short and sweet:
+    #   desc 'My App'
+    #   define 'myapp' do
+    #     . . .
+    #     doc projects('myapp:foo', 'myapp:bar')
+    #   end
+    class Javadoc < Base
+      
+      specify :language => :java, :source_ext => 'java'
+      
+      def generate(sources, target, options = {})
+        cmd_args = [ '-d', target, Buildr.application.options.trace ? '-verbose' : '-quiet' ]
+        options.reject { |key, value| [:sourcepath, :classpath].include?(key) }.
+          each { |key, value| value.invoke if value.respond_to?(:invoke) }.
+          each do |key, value|
+            case value
+            when true, nil
+              cmd_args << "-#{key}"
+            when false
+              cmd_args << "-no#{key}"
+            when Hash
+              value.each { |k,v| cmd_args << "-#{key}" << k.to_s << v.to_s }
+            else
+              cmd_args += Array(value).map { |item| ["-#{key}", item.to_s] }.flatten
+            end
+          end
+        [:sourcepath, :classpath].each do |option|
+          Array(options[option]).flatten.tap do |paths|
+            cmd_args << "-#{option}" << paths.flatten.map(&:to_s).join(File::PATH_SEPARATOR) unless paths.empty?
+          end
+        end
+        cmd_args += sources.flatten.uniq
+        unless Buildr.application.options.dryrun
+          info "Generating Javadoc for #{project.name}"
+          trace (['javadoc'] + cmd_args).join(' ')
+          Java.load
+          Java.com.sun.tools.javadoc.Main.execute(cmd_args.to_java(Java.java.lang.String)) == 0 or
+            fail 'Failed to generate Javadocs, see errors above'
+        end
+      end
+    end
+  end
+end
+
+Buildr::Doc.engines << Buildr::Doc::Javadoc
