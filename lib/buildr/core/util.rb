927078045c5f1c0cc89a3031207376b5e4372a39
Daniel Spiewak <djspiewak@apache.org>
2010-03-01 17:09:49 +0000

    Explicitly load JFFI backend on Windows (BUILDR-348)
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@917594 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/core/util.rb
+++ lib/buildr/core/util.rb
@@ -406,6 +406,9 @@ if Buildr::Util.java_platform?
 
   module FileUtils
     extend FFI::Library
+    
+    ffi_lib 'c' if Buildr::Util::win_os?
+    
     alias_method :__jruby_system__, :system
     attach_function :system, [:string], :int
     alias_method :__native_system__, :system
