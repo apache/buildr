7944732d912603e80840610b2ac06d36f9f14ac3
Alex Boisvert <boisvert@apache.org>
2010-09-17 17:10:30 +0000

    BUILDR-506 Gem packaging does not work under windows (Peter Donald)
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@998209 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/packaging/gems.rb
+++ lib/buildr/packaging/gems.rb
@@ -60,11 +60,10 @@ module Buildr
       spec.mark_version
       spec.validate
 
-      File.open(name, 'w') do |io|
+      File.open(name, 'wb') do |io|
         Gem::Package.open(io, 'w', nil) do |pkg|
           pkg.metadata = spec.to_yaml
           file_map.each do |path, content|
-            p "path #{path} content #{content}"
             next if content.nil? || File.directory?(content.to_s)
             pkg.add_file_simple(path, File.stat(content.to_s).mode & 0777, File.size(content.to_s)) do |os|
               File.open(content.to_s, "rb") do |file|
