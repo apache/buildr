a383f38478b9bca09b9dbf31a4433e45be096cf3
Antoine Toulme <toulmean@apache.org>
2010-09-17 22:30:04 +0000

    BUILDR-403: Buildr::Util::Gems.install does not find gems on remote sources
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@998357 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/core/util.rb
+++ lib/buildr/core/util.rb
@@ -142,8 +142,8 @@ module Buildr
       # is not running interactively (on a tty)
       def install(*dependencies)
         raise ArgumentError, "Expected at least one argument" if dependencies.empty?
-        remote = dependencies.map { |dep| Gem::SourceInfoCache.search(dep).last || dep }
-        not_found_deps, to_install = remote.partition { |gem| gem.is_a?(Gem::Dependency) }
+        remote = dependencies.map{ |dep| Gem.source_index.search(dep).last || Gem::SpecFetcher.fetcher.fetch( dep, true ).map{ |spec, source| spec }.last } 
+        not_found_deps, to_install = remote.partition { |gem| gem.is_a?(Gem::Dependency) || gem.nil? }
         fail Gem::LoadError, "Build requires the gems #{not_found_deps.join(', ')}, which cannot be found in local or remote repository." unless not_found_deps.empty?
         uses = "This build requires the gems #{to_install.map(&:full_name).join(', ')}:"
         fail Gem::LoadError, "#{uses} to install, run Buildr interactively." unless $stdout.isatty
