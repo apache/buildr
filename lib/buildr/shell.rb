780ba64778c859eb4c1b053c633792bdda22e33c
Alex Boisvert <boisvert@apache.org>
2010-10-01 05:25:03 +0000

    Fix previous commit
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@1003390 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/shell.rb
+++ lib/buildr/shell.rb
@@ -79,12 +79,12 @@ module Buildr
         unless @rebel_home
           @rebel_home = ENV['REBEL_HOME'] || ENV['JREBEL'] || ENV['JREBEL_HOME']
 
-          if @rebel_home && File.directory? @rebel_home
+          if @rebel_home and File.directory? @rebel_home
             @rebel_home += File::SEPARATOR + 'jrebel.jar'
           end
         end
 
-        if @rebel_home && File.exists? @rebel_home
+        if @rebel_home and File.exists? @rebel_home
           @rebel_home
         else
           nil
