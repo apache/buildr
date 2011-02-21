1941c689ed4c8dd9152f195cf3c7b4358f4af90b
Alex Boisvert <boisvert@apache.org>
2011-02-21 04:04:23 +0000

    Guard Windows-specific tests with Buildr::Util.win_os?
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@1072841 13f79535-47bb-0310-9956-ffa450edef68

--- spec/ide/idea_spec.rb
+++ spec/ide/idea_spec.rb
@@ -1019,30 +1019,31 @@ PROJECT_XML
     end
 
     describe "with local_repository_env_override = nil" do
-      describe "base_directory on different drive on windows" do
-        before do
-          @foo = define "foo", :base_dir => "C:/bar" do
-            iml.local_repository_env_override = nil
+      if Buildr::Util.win_os?
+        describe "base_directory on different drive on windows" do
+          before do
+            @foo = define "foo", :base_dir => "C:/bar" do
+              iml.local_repository_env_override = nil
+            end
+          end
+
+          it "generates relative paths correctly" do
+            @foo.iml.send(:resolve_path, "E:/foo").should eql('E:/foo')
           end
         end
 
-        it "generates relative paths correctly" do
-          @foo.iml.send(:resolve_path, "E:/foo").should eql('E:/foo')
-        end
-      end
+        describe "base_directory on same drive on windows" do
+          before do
+            @foo = define "foo", :base_dir => "C:/bar" do
+              iml.local_repository_env_override = nil
+            end
+          end
 
-      describe "base_directory on same drive on windows" do
-        before do
-          @foo = define "foo", :base_dir => "C:/bar" do
-            iml.local_repository_env_override = nil
+          it "generates relative paths correctly" do
+            @foo.iml.send(:resolve_path, "C:/foo").should eql('$MODULE_DIR$/../foo')
           end
         end
-
-        it "generates relative paths correctly" do
-          @foo.iml.send(:resolve_path, "C:/foo").should eql('$MODULE_DIR$/../foo')
-        end
       end
-
     end
   end
 
