b39ccfe94303a214c2bd0d8323f73e440b5ad69f
Antoine Toulme <toulmean@apache.org>
2011-06-23 07:53:11 +0000

    contributing a few more integration tests that can also be used for documentation. Also, cleaning up the integration tests, to make them easier to write hopefully
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@1138755 13f79535-47bb-0310-9956-ffa450edef68

--- tests/integration_testing.rb
+++ tests/integration_testing.rb
@@ -13,44 +13,69 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-BUILDR = ENV['BUILDR'] || File.join(File.expand_path(File.dirname(__FILE__)), "..", "_buildr")
+BUILDR = ENV['BUILDR'] || File.expand_path("../_buildr", File.dirname(__FILE__))
 
 require 'test/unit'
+require 'zip/zip'
 
-def test(folder, cmd)
-  eval <<-TEST
-class #{folder.sub("-", "").capitalize} < Test::Unit::TestCase
+module Buildr
+  module IntegrationTests
+    
+    def self.test(folder, cmd, after_block = nil)
+      
+      eval <<-TEST
+      class #{folder.sub("-", "").capitalize} < Test::Unit::TestCase
 
-  def test_#{folder.sub("-", "")}
-    result = %x[cd #{File.join(File.expand_path(File.dirname(__FILE__)), "#{folder}")} ; #{cmd} ; #{BUILDR} clean]
+        def test_#{folder.sub("-", "")}
+          begin
+            result = %x[cd #{File.expand_path("#{folder}", File.dirname(__FILE__))} ; #{BUILDR} #{cmd}]
+            assert($?.success?)
+            #{ after_block || "" }
+          ensure
+            %x[cd #{File.expand_path("#{folder}", File.dirname(__FILE__))} ; #{BUILDR} clean]
+          end
+
+        end
+
+      end  
+      TEST
+
+    end
+
+    #BUILDR-320 still not resolved.
+    #test "BUILDR-320", "--trace -P"
+    
+    test "JavaSystemProperty", "test"
+    
+    test "helloWorld", "package"
+    
+    test "compile_with_parent", "compile"
+    
+    test "junit3", "test"
+    
+    test "include_path", "package", <<-CHECK
+path = File.expand_path("include_path/target/proj-1.0.zip", File.dirname(__FILE__))
+assert(File.exist? path)
+Zip::ZipFile.open(path) {|zip|
+assert(!zip.get_entry("distrib/doc/index.html").nil?)
+assert(!zip.get_entry("distrib/lib/slf4j-api-1.6.1.jar").nil?)
+}
+    CHECK
+    
+    test "include_as", "package", <<-CHECK
+path = File.expand_path("include_as/target/proj-1.0.zip", File.dirname(__FILE__))
+assert(File.exist? path)
+Zip::ZipFile.open(path) {|zip|
+assert(!zip.get_entry("docu/index.html").nil?)
+assert(!zip.get_entry("lib/logging.jar").nil?)
+}
+    CHECK
+    
+    test "package_war_as_jar", "package", <<-CHECK
+    assert(File.exist? File.join(File.expand_path(File.dirname(__FILE__)), "package_war_as_jar", "target", "webapp-1.0.jar"))
+    %x[cd #{File.expand_path("package_war_as_jar", File.dirname(__FILE__))} ; #{BUILDR} clean]
     assert($?.success?)
-  end
+    CHECK
 
-end  
-TEST
-  
-end
-
-class Buildr320 < Test::Unit::TestCase
-   
-   def test_circular_dependency
-     result = %x[cd #{File.join(File.expand_path(File.dirname(__FILE__)), "BUILDR-320")} ; #{BUILDR} --trace -P]
-     assert($?.success?)
-   end   
-end
-
-test("JavaSystemProperty", "#{BUILDR} test")
-test("helloWorld", "#{BUILDR} package")
-test("compile_with_parent", "#{BUILDR} compile")
-test("junit3", "#{BUILDR} test")
-
-class Package_war_as_jar < Test::Unit::TestCase
-  
-  def test_war_extension_is_jar
-    result = %x[cd #{File.join(File.expand_path(File.dirname(__FILE__)), "package_war_as_jar")} ; #{BUILDR} package]
-    assert($?.success?)
-    assert(File.exist? File.join(File.expand_path(File.dirname(__FILE__)), "package_war_as_jar", "target", "webapp-1.0.jar")) 
-    %x[cd #{File.join(File.expand_path(File.dirname(__FILE__)), "package_war_as_jar")} ; #{BUILDR} clean]
-    assert($?.success?)
   end
 end
