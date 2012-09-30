843c2aeff42c1e951aa7cf1aed91e8aff3a7889d
Peter Donald <donaldp@apache.org>
2012-09-30 22:21:15 +0000

    Extract JOB_NAME into a local eviroment variable BUILD_TASK
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@1392121 13f79535-47bb-0310-9956-ffa450edef68

--- ci.sh
+++ ci.sh
@@ -1,48 +1,49 @@
 #!/usr/bin/env bash
 
 export SCALA_HOME=/home/hudson/tools/scala-2.9.0.1 ;
+export BUILD_TASK=$JOB_NAME
 
-if [ "X$JOB_NAME" == "XBuildr-ci-build" ]; then
+if [ "X$BUILD_TASK" == "XBuildr-ci-build" ]; then
   export JAVA_HOME=/home/hudson/tools/java/latest1.6-64 ;
   source .rvmrc
   rvm "${BUILDR_RUBY_VERSION}@${BUILDR_GEMSET}" exec rake ci --trace 2>&1
-elif [ "X$JOB_NAME" == "XBuildr-ci-build-1.9" ]; then
+elif [ "X$BUILD_TASK" == "XBuildr-ci-build-1.9" ]; then
   export JAVA_HOME=/home/hudson/tools/java/latest1.6-64 ;
   export BUILDR_RUBY_VERSION=ruby-1.9.2-p320
   source .rvmrc
   rvm "${BUILDR_RUBY_VERSION}@${BUILDR_GEMSET}" exec rake ci --trace 2>&1
-elif [ "X$JOB_NAME" == "XBuildr-ci-build-1.8" ]; then
+elif [ "X$BUILD_TASK" == "XBuildr-ci-build-1.8" ]; then
   export JAVA_HOME=/home/hudson/tools/java/latest1.6-64 ;
   export BUILDR_RUBY_VERSION=ruby-1.8.7-p358
   source .rvmrc
   rvm "${BUILDR_RUBY_VERSION}@${BUILDR_GEMSET}" exec rake ci --trace 2>&1
-elif [ "X$JOB_NAME" == "XBuildr-ci-build-jruby-jdk1.5" ]; then
+elif [ "X$BUILD_TASK" == "XBuildr-ci-build-jruby-jdk1.5" ]; then
   export BUILDR_RUBY_VERSION=jruby-1.6.7
   export JAVA_HOME=/home/hudson/tools/java/latest1.5-64 ;
   source .rvmrc
   rvm "${BUILDR_RUBY_VERSION}@${BUILDR_GEMSET}" exec rake ci --trace 2>&1
-elif [ "X$JOB_NAME" == "XBuildr-ci-build-jruby-jdk1.6" ]; then
+elif [ "X$BUILD_TASK" == "XBuildr-ci-build-jruby-jdk1.6" ]; then
   export BUILDR_RUBY_VERSION=jruby-1.6.7
   export JAVA_HOME=/home/hudson/tools/java/latest1.6-64 ;
   source .rvmrc
   rvm "${BUILDR_RUBY_VERSION}@${BUILDR_GEMSET}" exec rake ci --trace 2>&1
-elif [ "X$JOB_NAME" == "XBuildr-ci-build-jruby-jdk1.7" ]; then
+elif [ "X$BUILD_TASK" == "XBuildr-ci-build-jruby-jdk1.7" ]; then
   export BUILDR_RUBY_VERSION=jruby-1.6.7
   export JAVA_HOME=/home/hudson/tools/java/latest1.7-64 ;
   source .rvmrc
   rvm "${BUILDR_RUBY_VERSION}@${BUILDR_GEMSET}" exec rake ci --trace 2>&1
-elif [ "X$JOB_NAME" == "XBuildr-metrics-build" ]; then
+elif [ "X$BUILD_TASK" == "XBuildr-metrics-build" ]; then
   export BUILDR_RUBY_VERSION=ruby-1.9.2-p320
   export JAVA_HOME=/home/hudson/tools/java/latest1.6-64 ;
   source .rvmrc
   rvm "${BUILDR_RUBY_VERSION}@${BUILDR_GEMSET}" exec rake coverage metrics --trace 2>&1
-elif [ "X$JOB_NAME" == "XBuildr-website-build" ]; then
+elif [ "X$BUILD_TASK" == "XBuildr-website-build" ]; then
   export BUILDR_RUBY_VERSION=ruby-1.8.7-p358
   export JAVA_HOME=/home/hudson/tools/java/latest1.6-64 ;
   export PATH=$PATH:/home/toulmean/prince/bin
   source .rvmrc
   rvm "${BUILDR_RUBY_VERSION}@${BUILDR_GEMSET}" exec rake jekyll --trace 2>&1
-elif [ "X$JOB_NAME" == "XBuildr-omnibus-build" ]; then
+elif [ "X$BUILD_TASK" == "XBuildr-omnibus-build" ]; then
   export BUILDR_RUBY_VERSION=ruby-1.9.2-p320
   export JAVA_HOME=/home/hudson/tools/java/latest1.7-64 ;
   source .rvmrc
