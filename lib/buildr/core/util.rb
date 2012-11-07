b45cde8012b25ca60e34b117414879870e92923c
Peter Donald <donaldp@apache.org>
2012-11-07 09:58:08 +0000

    Remove monkey-patch as JRUBY-3381 was resolved in JRuby 1.6RC1.
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@1406514 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/core/util.rb
+++ lib/buildr/core/util.rb
@@ -285,27 +285,6 @@ if Buildr::Util.java_platform?
     remove_method :error if method_defined?(:error)
   end
 
-  # Fix for BUILDR-292.
-  # JRuby fails to rename a file on different devices
-  # this monkey-patch wont be needed when JRUBY-3381 gets resolved.
-  module FileUtils #:nodoc:
-    alias_method :__mv_native, :mv
-
-    def mv(from, to, options = nil)
-      dir_to = File.directory?(to) ? to : File.dirname(to)
-      Array(from).each do |from|
-        dir_from = File.dirname(from)
-        if File.stat(dir_from).dev != File.stat(dir_to).dev
-          cp from, to, options
-          rm from, options
-        else
-          __mv_native from, to, options
-        end
-      end
-    end
-    private :mv
-  end
-
   module RakeFileUtils #:nodoc:
     def rake_merge_option(args, defaults)
       defaults[:verbose] = false if defaults[:verbose] == Rake::FileUtilsExt::DEFAULT
