c199c8d41591a293592d6600c7a3f12f15994ddf
Peter Donald <donaldp@apache.org>
2013-01-11 04:49:54 +0000

    Ensure that the order of the attributes in the underlying serialized xml matches those generated by IDEA if running under ruby 1.9+ by explicitly ordering inserts into hash
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@1431880 13f79535-47bb-0310-9956-ffa450edef68

--- lib/buildr/ide/idea.rb
+++ lib/buildr/ide/idea.rb
@@ -757,12 +757,13 @@ module Buildr
       end
 
       def project_root_manager_component
-        attribs = {"version" => "2",
-                   "assert-keyword" => "true",
-                   "jdk-15" => "true",
-                   "project-jdk-name" => self.jdk_version,
-                   "project-jdk-type" => "JavaSDK",
-                   "languageLevel" => "JDK_#{self.jdk_version.gsub('.', '_')}"}
+        attribs = {}
+        attribs["version"] = "2"
+        attribs["languageLevel"] = "JDK_#{self.jdk_version.gsub('.', '_')}"
+        attribs["assert-keyword"] = "true"
+        attribs["jdk-15"] = "true"
+        attribs["project-jdk-name"] = self.jdk_version
+        attribs["project-jdk-type"] = "JavaSDK"
         create_component("ProjectRootManager", attribs) do |xml|
           xml.output("url" => "file://$PROJECT_DIR$/out")
         end
