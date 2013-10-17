d1df422883aa77b32b85fa98f171931d72082498
Peter Donald <donaldp@apache.org>
2013-10-17 17:02:10 +0000

    Ensure that the directory for generated source code is added to the idea project directory
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@1533157 13f79535-47bb-0310-9956-ffa450edef68

--- addon/buildr/jaxb_xjc.rb
+++ addon/buildr/jaxb_xjc.rb
@@ -50,6 +50,8 @@ module Buildr
       target_dir = File.expand_path(options[:directory] || _(:target, :generated, :jaxb, 'main/java'))
       timestamp_file = File.expand_path("#{target_dir}/jaxb-#{options[:id] || 1}.cache")
 
+      project.iml.main_source_directories << target_dir if project.iml?
+
       file(target_dir => timestamp_file)
 
       file(timestamp_file => files.flatten) do |task|
