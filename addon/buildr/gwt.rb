1ac8c2110e50de1d8899759c0669de51512d4ac2
Peter Donald <donaldp@apache.org>
2014-01-18 21:33:03 +0000

    Include the compiled classes on path for GWT addon to ensure the GWT compiler can load annotations.
    git-svn-id: https://svn.apache.org/repos/asf/buildr/trunk@1559423 13f79535-47bb-0310-9956-ffa450edef68

--- addon/buildr/gwt.rb
+++ addon/buildr/gwt.rb
@@ -102,7 +102,7 @@ module Buildr
       def gwt(module_names, options = {})
         output_key = options[:output_key] || project.id
         output_dir = project._(:target, :generated, :gwt, output_key)
-        artifacts = (project.compile.sources + project.resources.sources).collect do |a|
+        artifacts = ([project.compile.target] + project.compile.sources + project.resources.sources).collect do |a|
           a.is_a?(String) ? file(a) : a
         end
         dependencies = options[:dependencies] ? artifacts(options[:dependencies]) : (project.compile.dependencies + [project.compile.target]).collect do |dep|
