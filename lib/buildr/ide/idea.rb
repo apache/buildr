af4bdbc766a31f33a627cc593ae4893882f89972
Peter Donald <peter@realityforge.org>
2014-11-11 08:49:05 +1100

    Update ipr.add_gwt_configuration method to support GWT 2.7 configuration parameters and IDEA 14 parameters.

--- lib/buildr/ide/idea.rb
+++ lib/buildr/ide/idea.rb
@@ -905,19 +905,35 @@ module Buildr #:nodoc:
         end
       end
 
-      def add_gwt_configuration(launch_page, project, options = {})
-        name = options[:name] || "Run #{launch_page}"
-        shell_parameters = options[:shell_parameters] || ''
+      def add_gwt_configuration(project, options = {})
+        launch_page = options[:launch_page]
+        name = options[:name] || (launch_page ? "Run #{launch_page}" : "Run #{project.name} DevMode")
+        shell_parameters = options[:shell_parameters]
         vm_parameters = options[:vm_parameters] || '-Xmx512m'
+        singleton = options[:singleton].nil? ? true : !!options[:singleton]
+        super_dev = options[:super_dev].nil? ? true : !!options[:super_dev]
 
-        add_configuration(name, 'GWT.ConfigurationType', 'GWT Configuration') do |xml|
+        start_javascript_debugger = options[:start_javascript_debugger].nil? ? true : !!options[:start_javascript_debugger]
+
+        add_configuration(name, 'GWT.ConfigurationType', 'GWT Configuration', false, :singleton => singleton) do |xml|
           xml.module(:name => project.iml.id)
-          xml.option(:name => 'RUN_PAGE', :value => launch_page)
-          xml.option(:name => 'SHELL_PARAMETERS', :value => shell_parameters)
+
           xml.option(:name => 'VM_PARAMETERS', :value => vm_parameters)
+          xml.option(:name => 'RUN_PAGE', :value => launch_page) if launch_page
+
+          xml.option(:name => 'START_JAVASCRIPT_DEBUGGER', :value => start_javascript_debugger)
+          xml.option(:name => 'USE_SUPER_DEV_MODE', :value => super_dev)
+          xml.option(:name => 'SHELL_PARAMETERS', :value => shell_parameters) if shell_parameters
+
+          xml.RunnerSettings(:RunnerId => 'Debug') do |xml|
+            xml.option(:name => 'DEBUG_PORT', :value => '')
+            xml.option(:name => 'TRANSPORT', :value => 0)
+            xml.option(:name => 'LOCAL', :value => true)
+          end
 
           xml.RunnerSettings(:RunnerId => 'Run')
           xml.ConfigurationWrapper(:RunnerId => 'Run')
+          xml.ConfigurationWrapper(:RunnerId => 'Debug')
           xml.method()
         end
       end
