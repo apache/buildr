4fca5593b5cb05902d1cf880e9517456b280e5dd
Peter Donald <peter@realityforge.org>
2014-11-14 09:49:35 +1100

    Add missing gwt dependencies to idea module files if they are not already present

--- addon/buildr/gwt.rb
+++ addon/buildr/gwt.rb
@@ -125,6 +125,20 @@ module Buildr
 
         version = gwt_detect_version(dependencies) || Buildr::GWT.version
 
+        if project.iml?
+
+          existing_deps = project.compile.dependencies.collect do |d|
+            a = artifact(d)
+            a.invoke if a.respond_to?(:invoke)
+            a.to_s
+          end
+          Buildr::GWT.dependencies(version).each do |d|
+            a = artifact(d)
+            a.invoke if a.respond_to?(:invoke)
+            project.iml.main_dependencies << a.to_s unless existing_deps.include?(a.to_s)
+          end
+        end
+
         task = project.file(output_dir) do
           Buildr::GWT.gwtc_main(module_names,
                                 (dependencies + artifacts).flatten.compact,
