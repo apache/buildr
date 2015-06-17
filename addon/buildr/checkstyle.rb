1dbc8bca8209ef010f543da4501431735ac82793
Peter Donald <peter@realityforge.org>
2015-06-17 22:21:29 +1000

    Update the checkstyle addon to use checkstyle 6.7.

--- addon/buildr/checkstyle.rb
+++ addon/buildr/checkstyle.rb
@@ -23,13 +23,13 @@ module Buildr
       # The specs for requirements
       def dependencies
         [
-          'com.puppycrawl.tools:checkstyle:jar:6.6',
-          'antlr:antlr:jar:2.7.7',
+          'com.puppycrawl.tools:checkstyle:jar:6.7',
           'org.antlr:antlr4-runtime:jar:4.5',
+          'antlr:antlr:jar:2.7.7',
           'com.google.guava:guava:jar:18.0',
           'org.apache.commons:commons-lang3:jar:3.4',
           'org.abego.treelayout:org.abego.treelayout.core:jar:1.0.1',
-          'commons-cli:commons-cli:jar:1.2',
+          'commons-cli:commons-cli:jar:1.3',
           'commons-beanutils:commons-beanutils-core:jar:1.8.3',
           'commons-logging:commons-logging:jar:1.1.1'
         ]
