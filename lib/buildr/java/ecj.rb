ac9ab5bf90f3b75d0e2f19fb21172a84350695bd
Antoine Toulme <antoine@lunar-ocean.com>
2016-08-11 23:14:34 -0700

    Bring back ECJ into the fold - add its dependencies to classpath, as scala and other compilers do. We can sort this out later

--- lib/buildr/java/ecj.rb
+++ lib/buildr/java/ecj.rb
@@ -42,7 +42,6 @@ module Buildr #:nodoc:
         cmd_args = []
         # tools.jar contains the Java compiler.
         dependencies << Java.tools_jar if Java.tools_jar
-        dependencies << Buildr::Compiler::Ecj.dependencies
         cmd_args << '-classpath' << ('"' + dependencies.join(File::PATH_SEPARATOR) + '"') unless dependencies.empty?
         source_paths = sources.select { |source| File.directory?(source) }
         cmd_args << '-sourcepath' << source_paths.join(File::PATH_SEPARATOR) unless source_paths.empty?
@@ -79,4 +78,5 @@ module Buildr #:nodoc:
   end
 end
 
+Java.classpath << lambda { Buildr::Compiler::Ecj.dependencies }
 Buildr::Compiler.compilers << Buildr::Compiler::Ecj
