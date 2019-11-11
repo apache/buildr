7c11c3bcf6cc1e942a2a4d21962af5409f809672
Peter Donald <peter@realityforge.org>
2019-11-11 13:04:57 +1100

    Add support for the GWT version `2.8.2-v20191108` released with the nonstandard group id

--- addon/buildr/gwt.rb
+++ addon/buildr/gwt.rb
@@ -146,6 +146,44 @@ module Buildr
               org.eclipse.jetty:jetty-xml:jar:9.2.14.v20151106
               org.eclipse.jetty.toolchain:jetty-schemas:jar:3.1.M0
           ) + validation_deps
+        elsif v == '2.8.2-v20191108'
+          %w(
+              org.realityforge.com.google.jsinterop:jsinterop-annotations:jar:2.8.2-v20191108
+              org.realityforge.com.google.jsinterop:jsinterop-annotations:jar:sources:2.8.2-v20191108
+              org.w3c.css:sac:jar:1.3
+              org.realityforge.com.google.gwt:gwt-dev:jar:2.8.2-v20191108
+              org.realityforge.com.google.gwt:gwt-user:jar:2.8.2-v20191108
+              com.google.code.gson:gson:jar:2.6.2
+
+              org.ow2.asm:asm:jar:7.1
+              org.ow2.asm:asm-util:jar:7.1
+              org.ow2.asm:asm-tree:jar:7.1
+              org.ow2.asm:asm-commons:jar:7.1
+
+              colt:colt:jar:1.2.0
+              ant:ant:jar:1.6.5
+              commons-collections:commons-collections:jar:3.2.2
+              commons-io:commons-io:jar:2.4
+              com.ibm.icu:icu4j:jar:63.1
+              tapestry:tapestry:jar:4.0.2
+
+              javax.annotation:javax.annotation-api:jar:1.2
+              javax.servlet:javax.servlet-api:jar:3.1.0
+              org.eclipse.jetty:jetty-annotations:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-continuation:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-http:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-io:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-jndi:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-plus:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-security:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-server:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-servlet:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-servlets:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-util:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-webapp:jar:9.2.14.v20151106
+              org.eclipse.jetty:jetty-xml:jar:9.2.14.v20151106
+              org.eclipse.jetty.toolchain:jetty-schemas:jar:3.1.M0
+          ) + validation_deps
         else
           raise "Unknown GWT version #{v}"
         end
@@ -320,7 +358,7 @@ module Buildr
         dependencies.each do |dep|
           if dep.respond_to?(:to_spec_hash)
             hash = dep.to_spec_hash
-            if 'com.google.gwt' == hash[:group] && 'gwt-user' == hash[:id] && :jar == hash[:type]
+            if %w(org.realityforge.com.google.gwt com.google.gwt).include?(hash[:group]) && 'gwt-user' == hash[:id] && :jar == hash[:type]
               version = hash[:version]
             end
           end
